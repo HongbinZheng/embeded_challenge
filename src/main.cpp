6f4bb3970daf0c27903499323cc61bc556c07faa
HongbinZheng <caffreybun@hotmail.com>
2022-12-04 21:43:20 -0500

    update stop breathing logic

--- src/main.cpp
+++ src/main.cpp
@@ -7,11 +7,12 @@ int main()
   double humidity;
   double temperature;
   DigitalOut led(LED1);
-
+  DigitalOut led2(LED2);
   while(1)
   { 
     while(aht10_read_data(i2c, humidity, temperature));
     cout<<"Humidity: "<<humidity<<"%    Temperature: "<<temperature<<"°C"<<endl;
+
 
     // if humidity is too low, maybe this person stops breathing
     if(humidity < 40)
@@ -19,13 +20,30 @@ int main()
       double humidity_buffer[10];
       int len_hb = sizeof(humidity_buffer) / sizeof(humidity_buffer[0]);
       // start recording the next 10 datapoints(0.5s per datapoint, total period of 5s)
-      for(int i = 0; i < len_hb; i++)
-      {
+      int index = 0;
+      while(index < len_hb){
         while(aht10_read_data(i2c, humidity, temperature));
         cout<<"Humidity: "<<humidity<<"%    Temperature: "<<temperature<<"°C"<<endl;
-        humidity_buffer[i] = humidity;
+        // humidity drop might be causing by breath in 
+        // or the person resume breathing
+        // reset the index so it can restart the recording when next stop breathing happen 
+        if(humidity > 40){
+          index =0;
+          continue;
+        }
+        humidity_buffer[index] = humidity;
+        index++;
         thread_sleep_for(500);
       }
+      
+      // for(int i = 0; i < len_hb; i++)
+      // {
+      //   while(aht10_read_data(i2c, humidity, temperature));
+      //   cout<<"Humidity: "<<humidity<<"%    Temperature: "<<temperature<<"°C"<<endl;
+      //   humidity_buffer[i] = humidity;
+      //   thread_sleep_for(500);
+      // }
+
       // calculate the average humidity, if it's still low, this person stops breathing, need to send a warning
       double sum = 0;
       for(int i = 0; i < len_hb; i++)
@@ -38,8 +56,14 @@ int main()
         while(1)
         {
           led = 1;
+          led2 = 0;
+          thread_sleep_for(300);
+          led = 0;
+          led2 = 1;
+          thread_sleep_for(300);
         }
       }
+
     }
     thread_sleep_for(500);
   }
