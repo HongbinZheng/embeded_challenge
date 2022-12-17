0cc42c5e1265ca4237a0a055ccbc3078cb22b980
HongbinZheng <caffreybun@hotmail.com>
2022-12-16 21:42:58 -0500

    update condition

--- src/main.cpp
+++ src/main.cpp
@@ -1,70 +1,47 @@
 #include "aht10.h"
 using namespace std;
 
+bool decrease(double pervious, double current){
+  if(current - pervious < 0)
+    return true;
+  return false;
+}
+
 int main()
 {
   I2C i2c(PB_7, PB_6);  // Datasheet Page 69: PB6 I2C1_SCL, PB7 I2C1_SDA
   double humidity;
   double temperature;
+  double pervious = 0.0;
   DigitalOut led(LED1);
   DigitalOut led2(LED2);
+  int index = 0;
   while(1)
   { 
     while(aht10_read_data(i2c, humidity, temperature));
     cout<<"Humidity: "<<humidity<<"%    Temperature: "<<temperature<<"°C"<<endl;
-
-
-    // if humidity is too low, maybe this person stops breathing
-    if(humidity < 40)
-    {
-      double humidity_buffer[10];
-      int len_hb = sizeof(humidity_buffer) / sizeof(humidity_buffer[0]);
-      // start recording the next 10 datapoints(0.5s per datapoint, total period of 5s)
-      int index = 0;
-      while(index < len_hb){
-        while(aht10_read_data(i2c, humidity, temperature));
-        cout<<"Humidity: "<<humidity<<"%    Temperature: "<<temperature<<"°C"<<endl;
-        // humidity drop might be causing by breath in 
-        // or the person resume breathing
-        // reset the index so it can restart the recording when next stop breathing happen 
-        if(humidity > 40){
-          index =0;
-          continue;
-        }
-        humidity_buffer[index] = humidity;
-        index++;
-        thread_sleep_for(500);
-      }
-      
-      // for(int i = 0; i < len_hb; i++)
-      // {
-      //   while(aht10_read_data(i2c, humidity, temperature));
-      //   cout<<"Humidity: "<<humidity<<"%    Temperature: "<<temperature<<"°C"<<endl;
-      //   humidity_buffer[i] = humidity;
-      //   thread_sleep_for(500);
-      // }
-
-      // calculate the average humidity, if it's still low, this person stops breathing, need to send a warning
-      double sum = 0;
-      for(int i = 0; i < len_hb; i++)
-      {
-        sum += humidity_buffer[i];
-      }
-      double average = sum/10;
-      if(average < 40)
-      {
-        while(1)
-        {
-          led = 1;
-          led2 = 0;
-          thread_sleep_for(300);
-          led = 0;
-          led2 = 1;
-          thread_sleep_for(300);
-        }
-      }
-
+    // if humidity is too low, maybe this person stops breathing,increase the index by one equals to + 0.5s
+    // or the current humidity is lower than pervious one, it might be stop breathing, increase the index, 
+    // if countinous decrease by 10s(20 datapoints), the person is stop breathing
+    // else the current humidity greater than previous, the decrease might becuase of breath in and breath out, reset the index.
+    if(humidity < 40 || humidity - pervious < 0){
+      index++;
+    }else{
+      index=0;
     }
+    if(index==19){
+      while(1)
+      {
+        led = 1;
+        led2 = 0;
+        thread_sleep_for(300);
+        led = 0;
+        led2 = 1;
+        thread_sleep_for(300);
+      }
+    }
+
+    pervious = humidity;
     thread_sleep_for(500);
   }
 }
